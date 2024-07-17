2182f5b59375e36e513df6d608d39c46d763f26a
TseX <xterminal@foxmail.com>
2024-07-17 09:48:50 +0800

    feat(frontend): add homepage timeline

---
+++ .vitepress/theme/layout/HomePage/ArticleGroup.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import StoryTimeline from './StoryTimeline.vue';
+
+const list = [{
+  date: 'July 16th, 2024',
+  title: 'HostsWith: \n A Simple Chrome Extension',
+  desc: '浏览器插件开发不完全指南，从入门到放弃！'
+}, {
+  date: 'July 16th, 2024',
+  title: 'Vue Uni Component',
+  desc: 'vue 跨版本通用组件库，提高开发效率，减少重复工作'
+}]
+
+</script>
+<template>
+  <section>
+    <h2 class="mb-3">
+      <div class="tracking-tight inline font-semibold text-4xl lg:text-6xl">Issues can be</div>
+      <div>
+        <!-- <div class="tracking-tight inline font-semibold text-4xl lg:text-6xl">be&nbsp;</div> -->
+        <div
+          class="tracking-tight inline font-semibold from-[#FF705B] to-[#FFB457] text-4xl lg:text-6xl bg-clip-text text-transparent bg-gradient-to-b">
+          weapons
+        </div>
+      </div>
+    </h2>
+    <div>
+      <StoryTimeline :list="list" />
+    </div>
+  </section>
+</template>
---
+++ .vitepress/theme/layout/HomePage/StoryTimeline.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import StoryCard from '../../../components/StoryCard/StoryCard.vue';
+interface TimelineItem {
+  date: string;
+  title: string;
+  desc: string;
+}
+defineProps<{
+  list: TimelineItem[]
+}>();
+</script>
+<template>
+  <ul class="flex">
+    <li class="md:w-96 w-72 px-3 pb-2 overflow-hidden" v-for="item in list">
+      <div
+        class="-ms-1 p-1 inline-flex items-center text-xs rounded-lg border border-transparent text-gray-500 hover:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700">
+        {{ item.date }}
+      </div>
+      <div class="relative w-full h-4 mb-2">
+        <div class="absolute -z-10 left-0 top-0 w-[110%] h-full flex items-center">
+          <!-- split-line -->
+          <div class="w-full h-[2px] bg-gray-100 dark:bg-neutral-700">
+          </div>
+        </div>
+        <div class="w-4 h-4 bg-gray-100 text-gray-800 rounded-full dark:bg-neutral-700 dark:text-white"></div>
+      </div>
+      <div class="w-full">
+        <StoryCard v-bind="item" hover-tilt />
+      </div>
+    </li>
+  </ul>
+</template>
