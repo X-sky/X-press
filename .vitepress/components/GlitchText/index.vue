d1364593a32a632085c603869a2e221c78541d4d
TseX <xterminal@foxmail.com>
2024-07-17 19:56:20 +0800

    feat(frontend): add glitch-text component

---
+++ .vitepress/components/GlitchText/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+// could be invalid due to prefers-reduced-motion setting
+import { ref } from 'vue';
+const clockRef = ref<HTMLDivElement>();
+defineProps<{
+  text: string
+}>();
+
+setTimeout(() => {
+  clockRef.value?.classList.remove('is-off');
+}, 2000)
+</script>
+<template>
+  <div class="screen glitch">
+    <div ref="clockRef" class="clock is-off">
+      <div :data-time="text" class="content">{{ text }}</div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
