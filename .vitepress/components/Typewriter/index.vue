bde03c820e7bc4d8ae06bc351d8ac0e106a283b6
TseX <xterminal@foxmail.com>
2024-07-18 14:34:17 +0800

    feat(frontend): add typewriter component * add typed.js deps

---
+++ .vitepress/components/Typewriter/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+
+import { onMounted, ref } from 'vue';
+import Typed from 'typed.js';
+
+const props = defineProps<{
+  textList: string[]
+}>();
+
+const textRef = ref<HTMLParagraphElement>();
+
+onMounted(() => {
+  if (textRef.value) {
+    new Typed(textRef.value, {
+      strings: props.textList,
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+})
+
+
+</script>
+<template>
+  <div class="flex">
+    <p ref="textRef"></p>
+  </div>
+</template>
