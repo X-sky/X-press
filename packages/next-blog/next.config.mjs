761d616e5ff4c0ec43cc789d33c03582e6c7f41d
TseX <xterminal@foxmail.com>
2024-07-29 21:30:16 +0800

    chore(frontend): add next-blog output config

--- packages/next-blog/next.config.mjs
+++ packages/next-blog/next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    trailingSlash: false,
-    compiler: {
-        removeConsole: process.env.NODE_ENV === 'production',
-    },
-    images: {
-        domains: ['avatars.githubusercontent.com','static.wixstatic.com','assets-global.website-files.com'],
-        
-    },
+  basePath: "/X-resume",
+  output: "export",
+  reactStrictMode: true,
+  trailingSlash: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "static.wixstatic.com",
+      "assets-global.website-files.com",
+    ],
+  },
 }
 
 export default nextConfig
